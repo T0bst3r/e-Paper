7fcc108d81f69d66afae3182f1bdca72df6c00dd
Tobias <leuteltt@gmail.com>
2021-08-01 17:03:20 +0200

    removing unneeded files

--- RaspberryPi_JetsonNano/python/examples/epd_4in2bc_test.py
+++ RaspberryPi_JetsonNano/python/examples/epd_4in2bc_test.py
@@ -17,30 +17,30 @@ logging.basicConfig(level=logging.DEBUG)
 
 try:
     logging.info("epd4in2bc Demo")
-    
+
     epd = epd4in2bc.EPD()
     logging.info("init and Clear")
     epd.init()
     epd.Clear()
     time.sleep(1)
-    
+
     # Drawing on the image
-    logging.info("Drawing")    
+    logging.info("Drawing")
     font24 = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 24)
     font18 = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 18)
-    
+
     # Drawing on the Horizontal image
-    logging.info("1.Drawing on the Horizontal image...") 
+    logging.info("1.Drawing on the Horizontal image...")
     HBlackimage = Image.new('1', (epd.width, epd.height), 255)  # 298*126
-    HRYimage = Image.new('1', (epd.width, epd.height), 255)  # 298*126  ryimage: red or yellow image  
+    HRYimage = Image.new('1', (epd.width, epd.height), 255)  # 298*126  ryimage: red or yellow image
     drawblack = ImageDraw.Draw(HBlackimage)
     drawry = ImageDraw.Draw(HRYimage)
     drawblack.text((10, 0), 'hello world', font = font24, fill = 0)
     drawblack.text((10, 20), '4.2inch e-Paper bc', font = font24, fill = 0)
-    drawblack.text((150, 0), u'微雪电子', font = font24, fill = 0)    
+    drawblack.text((150, 0), u'微雪电子', font = font24, fill = 0)
     drawblack.line((20, 50, 70, 100), fill = 0)
     drawblack.line((70, 50, 20, 100), fill = 0)
-    drawblack.rectangle((20, 50, 70, 100), outline = 0)    
+    drawblack.rectangle((20, 50, 70, 100), outline = 0)
     drawry.line((165, 50, 165, 100), fill = 0)
     drawry.line((140, 75, 190, 75), fill = 0)
     drawry.arc((140, 50, 190, 100), 0, 360, fill = 0)
@@ -48,14 +48,14 @@ try:
     drawry.chord((200, 50, 250, 100), 0, 360, fill = 0)
     epd.display(epd.getbuffer(HBlackimage), epd.getbuffer(HRYimage))
     time.sleep(2)
-    
+
     # Drawing on the Vertical image
     logging.info("2.Drawing on the Vertical image...")
     LBlackimage = Image.new('1', (epd.height, epd.width), 255)  # 126*298
     LRYimage = Image.new('1', (epd.height, epd.width), 255)  # 126*298
     drawblack = ImageDraw.Draw(LBlackimage)
     drawry = ImageDraw.Draw(LRYimage)
-    
+
     drawblack.text((2, 0), 'hello world', font = font18, fill = 0)
     drawblack.text((2, 20), '4.2inch epd bc', font = font18, fill = 0)
     drawblack.text((20, 50), u'微雪电子', font = font18, fill = 0)
@@ -69,33 +69,34 @@ try:
     drawry.chord((70, 150, 120, 200), 0, 360, fill = 0)
     epd.display(epd.getbuffer(LBlackimage), epd.getbuffer(LRYimage))
     time.sleep(2)
-    
+
     logging.info("3.read bmp file")
     HBlackimage = Image.open(os.path.join(picdir, '4in2b-b.bmp'))
-    HRYimage = Image.open(os.path.join(picdir, '4in2b-r.bmp'))
+    HRYimage = Image.new('1', (epd.width, epd.height), 255)
+    # HRYimage = Image.open(os.path.join(picdir, '4in2b-r.bmp'))
     # HBlackimage = Image.open(os.path.join(picdir, '4in2c-b.bmp'))
     # HRYimage = Image.open(os.path.join(picdir, '4in2c-y.bmp'))
     epd.display(epd.getbuffer(HBlackimage), epd.getbuffer(HRYimage))
-    time.sleep(2)
-    
+    time.sleep(200)
+
     logging.info("4.read bmp file on window")
     blackimage1 = Image.new('1', (epd.width, epd.height), 255)  # 298*126
-    redimage1 = Image.new('1', (epd.width, epd.height), 255)  # 298*126    
+    redimage1 = Image.new('1', (epd.width, epd.height), 255)  # 298*126
     newimage = Image.open(os.path.join(picdir, '100x100.bmp'))
-    blackimage1.paste(newimage, (50,10))    
+    blackimage1.paste(newimage, (50,10))
     epd.display(epd.getbuffer(blackimage1), epd.getbuffer(redimage1))
-    
+
     logging.info("Clear...")
     epd.init()
     epd.Clear()
-    
+
     logging.info("Goto Sleep...")
     epd.sleep()
-        
+
 except IOError as e:
     logging.info(e)
-    
-except KeyboardInterrupt:    
+
+except KeyboardInterrupt:
     logging.info("ctrl + c:")
     epd4in2bc.epdconfig.module_exit()
     exit()
